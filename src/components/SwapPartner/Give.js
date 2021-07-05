8a77ad54ec268dbef77f9d2aaade9bcaa4712771
Aishwarya <aishwaryaagrawal1211@gmail.com>
2021-07-05 16:17:58 +0530

    Repair

--- src/components/SwapPartner/Give.js
+++ src/components/SwapPartner/Give.js
@@ -66,8 +66,8 @@ const Give = ({ API_URL, rider, current, setRider, setCurrent, absent, setTask }
 		fetch(`${API_URL}/items/${rider.number}`)
 			.then((vehicle) => vehicle.json())
 			.then((item) => {
-				if(Number(item.batteryCharge)>Number(charge))
-					return alert("Battery Chaarge cannot increase")
+				if(Number(item.batteryCharge)<Number(charge))
+					return alert("Battery Charge cannot increase")
 				let sett = { ...item, soc: charge, newstate: station };
 				setCurrent(sett);
 				document.querySelector('#sId').value = '0';
