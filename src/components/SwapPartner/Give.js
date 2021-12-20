e4045475a4c373a5bbc38dc21b07e3d7b3342f61
AishwaryaAgr <46737374+AishwaryaAgr@users.noreply.github.com>
2021-12-20 21:25:22 +0530

    Update Give.js

--- src/components/SwapPartner/Give.js
+++ src/components/SwapPartner/Give.js
@@ -111,7 +111,7 @@ const Give = ({
 		const charge = document.querySelector("#charge").value;
 		if (!checkSoc(charge) || charge === "")
 			return alert("Charge Must be between 0 and 50");
-		let defaulter = [9971040622, 9534734424]
+		let defaulter = [9971040622, 9534734424, 9560879261]
 		const station = document.querySelector("#station").value;
 		// console.log(rider.number);
 		for(let i=0;i<defaulter.length;i++){
