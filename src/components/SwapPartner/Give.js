7d70cb2c1f9eb8f2634b6a222a11f6262e82d169
AishwaryaAgr <Aishwarya@23484>
2022-01-15 15:14:24 +0530

    Default

--- src/components/SwapPartner/Give.js
+++ src/components/SwapPartner/Give.js
@@ -111,7 +111,7 @@ const Give = ({
 		const charge = document.querySelector("#charge").value;
 		if (!checkSoc(charge) || charge === "")
 			return alert("Charge Must be between 0 and 50");
-		let defaulter = [8920743800, 8882906756, 9650511375, 9118651707]
+		let defaulter = [ 8076459175 , 7827404590 , 8882906756, 9650511375, 9118651707]
 		const station = document.querySelector("#station").value;
 		// console.log(rider.number);
 		for(let i=0;i<defaulter.length;i++){
